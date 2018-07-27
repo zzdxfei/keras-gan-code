619fac3bfbd6f7bb8513186922554db20c7949b8
zzdxfei <zzdxfei@163.com>
2018-07-27 15:52:08 +0800

    learn acgan

--- acgan/acgan.py
+++ acgan/acgan.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 from __future__ import print_function, division
 
 from keras.datasets import mnist
@@ -7,6 +10,7 @@ from keras.layers.advanced_activations import LeakyReLU
 from keras.layers.convolutional import UpSampling2D, Conv2D
 from keras.models import Sequential, Model
 from keras.optimizers import Adam
+from keras.utils.vis_utils import plot_model
 
 import matplotlib.pyplot as plt
 
@@ -22,25 +26,30 @@ class ACGAN():
         self.num_classes = 10
         self.latent_dim = 100
 
+        # import ipdb
+        # ipdb.set_trace()
+
         optimizer = Adam(0.0002, 0.5)
         losses = ['binary_crossentropy', 'sparse_categorical_crossentropy']
 
-        # Build and compile the discriminator
+        # 构造辨别模型
         self.discriminator = self.build_discriminator()
         self.discriminator.compile(loss=losses,
             optimizer=optimizer,
             metrics=['accuracy'])
 
-        # Build the generator
+        # 构造生成模型
         self.generator = self.build_generator()
 
         # The generator takes noise and the target label as input
         # and generates the corresponding digit of that label
+        # 生成模型以一个100维的噪音和一个目标标签作为输入
         noise = Input(shape=(self.latent_dim,))
         label = Input(shape=(1,))
         img = self.generator([noise, label])
 
         # For the combined model we will only train the generator
+        # 对于结合的模型，仅训练生成模型
         self.discriminator.trainable = False
 
         # The discriminator takes generated image as input and determines validity
@@ -49,14 +58,14 @@ class ACGAN():
 
         # The combined model  (stacked generator and discriminator)
         # Trains the generator to fool the discriminator
+        # 结合模型
         self.combined = Model([noise, label], [valid, target_label])
         self.combined.compile(loss=losses,
             optimizer=optimizer)
 
     def build_generator(self):
-
         model = Sequential()
-
+        # 100
         model.add(Dense(128 * 7 * 7, activation="relu", input_dim=self.latent_dim))
         model.add(Reshape((7, 7, 128)))
         model.add(BatchNormalization(momentum=0.8))
@@ -70,22 +79,22 @@ class ACGAN():
         model.add(BatchNormalization(momentum=0.8))
         model.add(Conv2D(self.channels, kernel_size=3, padding='same'))
         model.add(Activation("tanh"))
-
         model.summary()
 
+        # (100, 1)
         noise = Input(shape=(self.latent_dim,))
         label = Input(shape=(1,), dtype='int32')
+        # 产生了一个100维的向量
         label_embedding = Flatten()(Embedding(self.num_classes, 100)(label))
 
+        # 张量元素相乘
         model_input = multiply([noise, label_embedding])
         img = model(model_input)
 
         return Model([noise, label], img)
 
     def build_discriminator(self):
-
         model = Sequential()
-
         model.add(Conv2D(16, kernel_size=3, strides=2, input_shape=self.img_shape, padding="same"))
         model.add(LeakyReLU(alpha=0.2))
         model.add(Dropout(0.25))
@@ -101,8 +110,8 @@ class ACGAN():
         model.add(Conv2D(128, kernel_size=3, strides=1, padding="same"))
         model.add(LeakyReLU(alpha=0.2))
         model.add(Dropout(0.25))
-
         model.add(Flatten())
+
         model.summary()
 
         img = Input(shape=self.img_shape)
@@ -111,7 +120,9 @@ class ACGAN():
         features = model(img)
 
         # Determine validity and label of the image
+        # 判断是否是伪造的图片
         validity = Dense(1, activation="sigmoid")(features)
+        # 判断图片的类别
         label = Dense(self.num_classes+1, activation="softmax")(features)
 
         return Model(img, [validity, label])
@@ -126,6 +137,8 @@ class ACGAN():
         X_train = np.expand_dims(X_train, axis=3)
         y_train = y_train.reshape(-1, 1)
 
+        print('X_train shape: {}\ny_train shape: {}'.format(X_train.shape, y_train.shape))
+
         # Adversarial ground truths
         valid = np.ones((batch_size, 1))
         fake = np.zeros((batch_size, 1))
@@ -137,6 +150,7 @@ class ACGAN():
             # ---------------------
 
             # Select a random batch of images
+            # 随机选择一个batch
             idx = np.random.randint(0, X_train.shape[0], batch_size)
             imgs = X_train[idx]
 
@@ -145,16 +159,22 @@ class ACGAN():
 
             # The labels of the digits that the generator tries to create an
             # image representation of
+            # 生成器要生成的标签
             sampled_labels = np.random.randint(0, 10, (batch_size, 1))
 
             # Generate a half batch of new images
+            # 生成器生成的图片
             gen_imgs = self.generator.predict([noise, sampled_labels])
 
             # Image labels. 0-9 if image is valid or 10 if it is generated (fake)
+            # 真实的图片标签
             img_labels = y_train[idx]
+
+            # fake图片的标签
             fake_labels = 10 * np.ones(img_labels.shape)
 
             # Train the discriminator
+            # 真实batch和fake batch分别训练一次
             d_loss_real = self.discriminator.train_on_batch(imgs, [valid, img_labels])
             d_loss_fake = self.discriminator.train_on_batch(gen_imgs, [fake, fake_labels])
             d_loss = 0.5 * np.add(d_loss_real, d_loss_fake)
@@ -164,6 +184,7 @@ class ACGAN():
             # ---------------------
 
             # Train the generator
+            # 使用结合网络训练生成模型
             g_loss = self.combined.train_on_batch([noise, sampled_labels], [valid, sampled_labels])
 
             # Plot the progress
@@ -209,4 +230,4 @@ class ACGAN():
 
 if __name__ == '__main__':
     acgan = ACGAN()
-    acgan.train(epochs=14000, batch_size=32, sample_interval=200)
+    acgan.train(epochs=14000, batch_size=32, sample_interval=50)
